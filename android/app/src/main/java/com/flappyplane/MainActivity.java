814850a6370afc027c1e74788baf838ba6c6ccc4
TuongThuy <thuyschannel2301@gmail.com>
2019-11-28 12:23:29 +0700

    update changed

--- android/app/src/main/java/com/flappyplane/MainActivity.java
+++ android/app/src/main/java/com/flappyplane/MainActivity.java
@@ -1,4 +1,4 @@
-package com.flappybird;
+package com.flappyplane;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FlappyBird";
+    return "FlappyPlane";
   }
 }
--- android/app/src/main/java/com/flappyplane/MainApplication.java
+++ android/app/src/main/java/com/flappyplane/MainApplication.java
@@ -1,4 +1,4 @@
-package com.flappybird;
+package com.flappyplane;
 
 import android.app.Application;
 import android.content.Context;
